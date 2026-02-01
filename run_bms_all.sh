174bc3290499825c4b42ff2435ae887640f26f31
rtvuser1 <tlubinski1@gmail.com>
2026-02-01 11:48:22 -0800

    Add ability to pass the -A option into the run_bms_all script

--- run_bms_all.sh
+++ run_bms_all.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-sbatch -G 1 run_bms.sh
-sbatch -G 2 run_bms.sh
-sbatch -G 4 run_bms.sh
-sbatch -G 8 run_bms.sh
-sbatch -G 16 run_bms.sh
-sbatch -G 32 run_bms.sh
-sbatch -G 64 run_bms.sh
-#sbatch -G 128 run_bms.sh
-#sbatch -G 256 run_bms.sh
+# Parse optional -A argument
+ACCOUNT_ARG=""
+while getopts "A:" opt; do
+  case $opt in
+    A) ACCOUNT_ARG="-A $OPTARG" ;;
+    *) echo "Usage: $0 [-A account]" >&2; exit 1 ;;
+  esac
+done
+
+sbatch -G 1 $ACCOUNT_ARG run_bms.sh
+sbatch -G 2 $ACCOUNT_ARG run_bms.sh
+sbatch -G 4 $ACCOUNT_ARG run_bms.sh
+sbatch -G 8 $ACCOUNT_ARG run_bms.sh
+sbatch -G 16 $ACCOUNT_ARG run_bms.sh
+sbatch -G 32 $ACCOUNT_ARG run_bms.sh
+sbatch -G 64 $ACCOUNT_ARG run_bms.sh
+#sbatch -G 128 $ACCOUNT_ARG run_bms.sh
+#sbatch -G 256 $ACCOUNT_ARG run_bms.sh
