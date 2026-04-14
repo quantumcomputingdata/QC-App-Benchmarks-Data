7a1024e697f7fb0db7d514c14d407c9568080947
rtvuser1 <tlubinski1@gmail.com>
2026-04-13 23:40:14 -0700

    Fix hamlib test script to use -gm not just -g to avoid ambiguity with -gpc

--- run_hamlib_test.sh
+++ run_hamlib_test.sh
@@ -21,28 +21,28 @@ appargs=(-a cudaq -c 1 -s 10000 -min $MIN -max $MAX -k 1 --time 0.1 -obs -m 4 $@
 
 # run using single GPU, without MPI, using SpinOperator
 echo "========================================================================="
-echo Running hamlib.hamlib_simulation_benchmark "${appargs[@]}" -g SpinOperator on one GPU
+echo Running hamlib.hamlib_simulation_benchmark "${appargs[@]}" -gm SpinOperator on one GPU
 
-python -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" -g SpinOperator -suffix "_g1" 2>&1 | tee hamlib.log
+python -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" -gm SpinOperator -suffix "_g1" 2>&1 | tee hamlib.log
 
 # run using single GPU, without MPI, using simple commuting groups
 echo "========================================================================="
-echo Running hamlib.hamlib_simulation_benchmark "${appargs[@]}" -g simple on one GPU
+echo Running hamlib.hamlib_simulation_benchmark "${appargs[@]}" -gm simple on one GPU
 
-python -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" -g simple -suffix "_g1" 2>&1 | tee hamlib.log
+python -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" -gm simple -suffix "_g1" 2>&1 | tee hamlib.log
 
 # ************ using MPI ************************
 
 # run using mgpu mode and MPI, sharing memory across GPUs for more qubits and faster execution, using SpinOperator
 echo "========================================================================="
-echo Running hamlib.hamlib_simulation_benchmark "${appargs[@]}" -g SpinOperator with MPI Shared GPUs
+echo Running hamlib.hamlib_simulation_benchmark "${appargs[@]}" -gm SpinOperator with MPI Shared GPUs
 
-srun -n $NUM_GPUS python -m mpi4py -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" -g SpinOperator -suffix "_g$NUM_GPUS" 2>&1 | tee hamlib.log
+srun -n $NUM_GPUS python -m mpi4py -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" -gm SpinOperator -suffix "_g$NUM_GPUS" 2>&1 | tee hamlib.log
 
 
 # run using parallel execution mode, using MPI, using simple commuting groups
 echo "========================================================================="
-echo Running hamlib.hamlib_simulation_benchmark "${appargs[@]}" -g simple on MPI parallel GPUs
+echo Running hamlib.hamlib_simulation_benchmark "${appargs[@]}" -gm simple on MPI parallel GPUs
 
-srun -n $NUM_GPUS python -m mpi4py -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" -g simple -gpc 1 -suffix "_g$NUM_GPUS" 2>&1 | tee hamlib.log
+srun -n $NUM_GPUS python -m mpi4py -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" -gm simple -gpc 1 -suffix "_g$NUM_GPUS" 2>&1 | tee hamlib.log
 
