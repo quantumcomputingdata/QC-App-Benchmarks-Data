d3c4691d73c5688064a631e881977c77a1153221
Tom Lubinski <rtvuser1@users.noreply.github.com>
2026-05-06 20:38:45 -0700

    Update srun_hamlib_m3.sh

--- srun_hamlib_m3.sh
+++ srun_hamlib_m3.sh
@@ -13,8 +13,8 @@ echo `date`: Executing HamLib Benchmark method 3 on $NUM_GPUS GPUs ...
 # pushd hamlib
 
 # use method 3 for fidelity measure
-#appargs=(-a cudaq -c 2 -w -s 1000 -min $MIN -max $MAX -k 2 -m 3 -ham condensedmatter/tfim/tfim -params 1D-grid:pbc,_h:2)
-appargs=(-a cudaq -c 2 -w -s 1000 -min $MIN -max $MAX -k 1 -m 3 -steps 10)
+#appargs=(-a cudaq -c 2 -s 1000 -min $MIN -max $MAX -k 2 -m 3 -ham condensedmatter/tfim/tfim -params 1D-grid:pbc,_h:2)
+appargs=(-a cudaq -c 2 -s 1000 -min $MIN -max $MAX -k 1 -m 3 -steps 10)
 
 echo srun -n $1 python -m mpi4py -m hamlib.hamlib_simulation_benchmark "${appargs[@]}" 
 
